39661e97ee835e9526e95693c2aeed4805c189ae
Nicolás Améstica Vidal <niamesvi3@gmail.com>
2026-09-23 18:04:17 -0300

    chore(release): actualizar version a 0.4.0 para dev

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -3,7 +3,7 @@
  */
 export const environment = {
   production: false,
-  version: 'v0.3.0',
+  version: 'v0.4.0',
   apiUrl: 'https://api.dev.girasindomito.cl',
   stage: 'dev',
   favoritesEnabled: true,
