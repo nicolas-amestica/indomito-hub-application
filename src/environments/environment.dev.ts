59ca7cb6884dd4a44c535dc0987f61d3bbc8d116
Nicolás Améstica Vidal <niamesvi3@gmail.com>
2026-09-25 21:57:10 -0300

    chore(release): actualizar version a 0.11.3 para dev

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -3,7 +3,7 @@
  */
 export const environment = {
   production: false,
-  version: 'v0.11.2',
+  version: 'v0.11.3',
   apiUrl: 'https://api.dev.girasindomito.cl',
   stage: 'dev',
   favoritesEnabled: true,
